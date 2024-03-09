79d4f488f3df0ce468ffff238df30f0325763cba
wentaohu <543469611@qq.com>
2024-03-10 00:12:27 +0800

    111

--- Script/Singleton/CursorManager.gd
+++ Script/Singleton/CursorManager.gd
@@ -21,6 +21,8 @@ var _clickPreStatus: CursorStatus;
 var _animationPlayer: AnimationPlayer;
 var isDrag: bool
 var _isClickLeft:bool=false
+var mouse_pos:Vector2
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _ready() -> void:
 	#Singleton = self;
@@ -28,10 +30,14 @@ func _ready() -> void:
 	#初始化 on-ready 变量
 	_animationPlayer = get_node("AnimationPlayer")
 	_animationPlayer.play(CursorStatus.find_key(_status))
-	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	mouse_pos = get_global_mouse_position()  
+	#Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+
 
 func _process(_delta: float) -> void:
-	global_position = get_global_mouse_position();
+	#global_position = get_global_mouse_position()  
+	#Input.warp_mouse(mouse_pos)
+	limitMousePos()
 
 func ChangeState(status: CursorStatus):
 	if isDrag:
@@ -43,6 +49,24 @@ func ChangeState(status: CursorStatus):
 
 func _input(event: InputEvent) -> void:
 	mouseShpaeManager(event)
+func limitMousePos():
+	mouse_pos = get_global_mouse_position()  
+	var window_size = DisplayServer.window_get_size()
+	#print(window_size,mouse_pos)
+	var max_x = window_size.x - 1  
+	var max_y = window_size.y - 1  
+	var min_x = 0  
+	var min_y = 0     
+# 如果鼠标试图超出窗口范围，将其位置限制在窗口内  
+	if mouse_pos.x > max_x:  
+		mouse_pos.x = max_x  
+	if mouse_pos.y > max_y:  
+		mouse_pos.y = max_y  
+	if mouse_pos.x < min_x:  
+		mouse_pos.x = min_x  
+	if mouse_pos.y < min_y:  
+		mouse_pos.y = min_y  
+	global_position = mouse_pos
 
 func mouseButtonLeftClickCheck(event: InputEvent) -> bool:
 	if event.button_index == MOUSE_BUTTON_LEFT:
